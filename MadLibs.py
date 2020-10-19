e71a120b379ffd6ecd44140e9f5284762b2612c4
ehuppe <ehuppe@potomacschool.org>
2020-10-19 12:57:31 -0400

    Create MadLibs.py

---
+++ MadLibs.py
@@ -0,0 +1,9 @@
+name1 = str(input("Enter a name: "))
+adj1 = str(input("Enter an adjective: "))
+name2 = str(input("Enter a name: "))
+place1 = str(input("Enter a place: "))
+adv1 = str(input("Enter an adverb: "))
+
+print(name1 + " the dog had a " + adj1 + " owner named " + name2 + ".")
+print(name2 + " bought the dog at " + place1 + ".")
+print(" They lived " + adv1 + " ever after.")
